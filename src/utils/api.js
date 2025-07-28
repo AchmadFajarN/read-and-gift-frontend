2d48d518a7e492e003225e139ca5e8975ee46eb8
ajay <achmadfajar712@gmail.com>
2025-07-28 18:07:03 +0700

    add environment

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
